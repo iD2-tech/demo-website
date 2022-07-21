d940dc9ac348153575110482348893ab7f884c65
DavyHyun <dhyun1124@gmail.com>
2022-07-21 00:15:00 -0700

    project setup / navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Order from "./pages/Order";
+import Cart from "./pages/Cart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" exact element={<Home />}/>
+                <Route path="/menu" exact element={<Menu />}/>
+                <Route path="/order" exact element={<Order />}/>
+                <Route path="/cart" exact element={<Cart />}/>
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose, AiFillShopping } from "react-icons/ai";
+
+
+import classes from "./Header.module.scss";
+import { Link, useNavigate } from "react-router-dom";
+
+const Header = () => {
+
+    const RESTNAME = "Wonderful Pho";
+
+    const navigation = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
+
+    const ctaClickHandler = () => {
+        menuToggleHandler();
+        navigation("/cart");
+    };
+
+    return (
+        <header className={classes.header}>
+            <div className={classes.header__content}>
+                <Link to="/" className={classes.header__content__logo}>
+                    {RESTNAME}
+                </Link>
+                <nav
+                    className={`${classes.header__content__nav} ${
+                        menuOpen && size.width < 768 ? classes.isMenu : ""
+                    }`}
+                >
+                    <ul>
+                        <li>
+                            <Link to="/menu" onClick={menuToggleHandler}>
+                                Menu
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/order" onClick={menuToggleHandler}>
+                                Order
+                            </Link>
+                        </li>
+                    </ul>
+                    <button onClick={ctaClickHandler}>
+                        <AiFillShopping size="2em" />
+                    </button>
+                </nav>
+                <div className={classes.header__content__toggle}>
+                    {!menuOpen ? (
+                        <BiMenuAltRight onClick={menuToggleHandler} />
+                    ) : (
+                        <AiOutlineClose onClick={menuToggleHandler} />
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
