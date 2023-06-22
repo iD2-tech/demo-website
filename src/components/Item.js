144ee457ac40ca50e6c8ffe6bc62a92f9878d73f
Johnslee123 <116469694+Johnslee123@users.noreply.github.com>
2023-06-21 22:36:19 -0700

    commented out davy code

--- src/components/Item.js
+++ src/components/Item.js
@@ -13,66 +13,66 @@ function Item(props) {
   const [selectedValue, setSelectedValue] = useState(0);
   const [triggerRerender, setTriggerRerender] = useState(0);
 
-  const openModal = (item) => {
-    setShow(true);
-    setSelected(item);
-    setSelectedValue(item.quantity || 1);
-  };
+  // const openModal = (item) => {
+  //   setShow(true);
+  //   setSelected(item);
+  //   setSelectedValue(item.quantity || 1);
+  // };
 
-  const closeModal = (item) => {
-    setShow(false);
+  // const closeModal = (item) => {
+  //   setShow(false);
 
-    let cartJSON = localStorage.getItem('cart');
-    cartJSON = JSON.parse(cartJSON);
-    if (cartJSON === null) {
-      cartJSON = [];
-    }
+  //   let cartJSON = localStorage.getItem('cart');
+  //   cartJSON = JSON.parse(cartJSON);
+  //   if (cartJSON === null) {
+  //     cartJSON = [];
+  //   }
 
-    let index = -1;
-    for (let cartParser = 0; cartParser < cartJSON.length; cartParser++) {
-      if (cartJSON[cartParser] !== null) {
-        if (cartJSON[cartParser].id === item.id) {
-          index = cartParser;
-          break;
-        }
-      }
-    }
+  //   let index = -1;
+  //   for (let cartParser = 0; cartParser < cartJSON.length; cartParser++) {
+  //     if (cartJSON[cartParser] !== null) {
+  //       if (cartJSON[cartParser].id === item.id) {
+  //         index = cartParser;
+  //         break;
+  //       }
+  //     }
+  //   }
 
-    const additionalQuantity = selectedValue;
+  //   const additionalQuantity = selectedValue;
 
-    if (index >= 0) {
-      const originalQuantity = cartJSON[index].quantity;
-      const updatedQuantity = originalQuantity + additionalQuantity;
-      cartJSON[index].quantity = updatedQuantity;
-    } else {
-      cartJSON.push({
-        default_price: item.default_price,
-        id: item.id,
-        images: item.images,
-        key: item.key,
-        name: item.name,
-        price: item.price,
-        quantity: additionalQuantity,
-      });
-    }
+  //   if (index >= 0) {
+  //     const originalQuantity = cartJSON[index].quantity;
+  //     const updatedQuantity = originalQuantity + additionalQuantity;
+  //     cartJSON[index].quantity = updatedQuantity;
+  //   } else {
+  //     cartJSON.push({
+  //       default_price: item.default_price,
+  //       id: item.id,
+  //       images: item.images,
+  //       key: item.key,
+  //       name: item.name,
+  //       price: item.price,
+  //       quantity: additionalQuantity,
+  //     });
+  //   }
 
-    for (let k = 0; k < cartJSON.length; k++) {
-      if (cartJSON[k] != null) {
-        if (cartJSON[k].quantity <= 0) {
-          delete cartJSON[k];
-        }
-      }
-    }
+  //   for (let k = 0; k < cartJSON.length; k++) {
+  //     if (cartJSON[k] != null) {
+  //       if (cartJSON[k].quantity <= 0) {
+  //         delete cartJSON[k];
+  //       }
+  //     }
+  //   }
 
-    const filtered = cartJSON.filter(function (el) {
-      return el != null;
-    });
+  //   const filtered = cartJSON.filter(function (el) {
+  //     return el != null;
+  //   });
 
-    localStorage.setItem('cart', JSON.stringify(filtered));
-    setTriggerRerender(triggerRerender + 1);
-    setSelectedValue(0);
-    window.dispatchEvent(new Event('storage'));
-  };
+  //   localStorage.setItem('cart', JSON.stringify(filtered));
+  //   setTriggerRerender(triggerRerender + 1);
+  //   setSelectedValue(0);
+  //   window.dispatchEvent(new Event('storage'));
+  // };
 
   const decreaseQuantity = () => {
     if (selectedValue > 0) {
@@ -84,67 +84,67 @@ function Item(props) {
     setSelectedValue((prevValue) => prevValue + 1);
   };
 
-  useEffect(() => {
-    window.dispatchEvent(new Event('storage'));
-    getProducts();
-    getPrices();
-    if (products.length === 0) {
-      if (counter < 1) {
-        setCounter(counter + 1);
-      }
-    }
-  }, [counter]);
+  // useEffect(() => {
+  //   window.dispatchEvent(new Event('storage'));
+  //   getProducts();
+  //   getPrices();
+  //   if (products.length === 0) {
+  //     if (counter < 1) {
+  //       setCounter(counter + 1);
+  //     }
+  //   }
+  // }, [counter]);
 
-  const getProducts = async () => {
-    fetch('http://localhost:3000/products')
-      .then((response) => response.json())
-      .then((data) => {
-        var array = data.products;
-        setProducts(array);
-      });
-  };
+  // const getProducts = async () => {
+  //   fetch('http://localhost:3000/products')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       var array = data.products;
+  //       setProducts(array);
+  //     });
+  // };
 
-  const getPrices = async () => {
-    fetch('http://localhost:3000/prices')
-      .then((response) => response.json())
-      .then((data) => {
-        var array = data.price;
-        setPrices(array);
-      });
-  };
+  // const getPrices = async () => {
+  //   fetch('http://localhost:3000/prices')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       var array = data.price;
+  //       setPrices(array);
+  //     });
+  // };
 
-  const combinePrices = () => {
-    for (let i = 0; i < products.length; i++) {
-      products[i].price = prices[i].unit_amount / 100;
-    }
-  };
+  // const combinePrices = () => {
+  //   for (let i = 0; i < products.length; i++) {
+  //     products[i].price = prices[i].unit_amount / 100;
+  //   }
+  // };
 
-  const displayMenu = (category) => {
-    combinePrices();
-    var array = [];
-    for (let i = 0; i < products.length; i++) {
-      if (products[i].name.includes(category)) {
-        array.push(products[i]);
-      }
-    }
+  // const displayMenu = (category) => {
+  //   combinePrices();
+  //   var array = [];
+  //   for (let i = 0; i < products.length; i++) {
+  //     if (products[i].name.includes(category)) {
+  //       array.push(products[i]);
+  //     }
+  //   }
 
-    var categoryName;
-    if (category.includes('Spring')) {
-      categoryName = 'Appetizers';
-    } else if (category.includes('Pho')) {
-      categoryName = 'Pho';
-    } else {
-      categoryName = 'Banh Mi';
-    }
+  //   var categoryName;
+  //   if (category.includes('Spring')) {
+  //     categoryName = 'Appetizers';
+  //   } else if (category.includes('Pho')) {
+  //     categoryName = 'Pho';
+  //   } else {
+  //     categoryName = 'Banh Mi';
+  //   }
 
-    nav('/displayMenu', {
-      state: {
-        productList: array,
-        category: categoryName,
-        allProducts: products,
-      },
-    });
-  };
+  //   nav('/displayMenu', {
+  //     state: {
+  //       productList: array,
+  //       category: categoryName,
+  //       allProducts: products,
+  //     },
+  //   });
+  // };
 
   return (
     <div>
