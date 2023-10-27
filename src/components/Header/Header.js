b392d92f18645871175c1399d0fe9e9ffe40627e
n1colezhou <nicolezhou2005@gmail.com>
2023-10-27 05:12:37 -0700

    mainly finished header. gradient needs work.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
-
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose, AiFillShopping } from "react-icons/ai";
-
-
 import classes from "./Header.module.scss";
 import { Link, useNavigate } from "react-router-dom";
 
-const Header = () => {
+const Header = () => { 
 
     // need global variable for restaurant name. Will be prop from JSON
     const RESTNAME = "COMMUNITY GROCERY AND DELI";
@@ -132,13 +129,6 @@ const Header = () => {
                         </span>
                     </button>
                 </nav>
-                <div className={classes.header__content__toggle}>
-                    {!menuOpen ? (
-                        <BiMenuAltRight onClick={menuToggleHandler} />
-                    ) : (
-                        <AiOutlineClose onClick={menuToggleHandler} />
-                    )}
-                </div>
             </div>
         </header>
     );
