89a23a4ed18923c4a3950c9475bcd779009fce83
Johnslee123 <116469694+Johnslee123@users.noreply.github.com>
2023-11-09 06:22:55 -0800

    aboutus visitus ui changes and header ui changes

--- src/components/AboutUsInfo/AboutUsInfo.js
+++ src/components/AboutUsInfo/AboutUsInfo.js
@@ -4,12 +4,12 @@ import classes from './AboutUsInfo.module.scss';
 const AboutUsInfo = ({ imageUrl, headerText, bodyText }) => {
     return (
         <div className={classes.about}>
+            <div className={classes.imageContain}>
+                <img src={imageUrl} className={classes.img} alt="About Us" />
+            </div>
             <div className={classes.text}>
                 <h className={classes.headerr}>{headerText}</h>
                 <h className={classes.body}>{bodyText}</h>
-            </div>
-            <div className={classes.imageContain}>
-                <img src={imageUrl} className={classes.img} alt="About Us" />
             </div>
         </div>
     );
