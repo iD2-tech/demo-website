93d2683bac6396034c568b8e107e85ddf2550942
DavyHyun <dhyun1124@gmail.com>
2023-11-14 12:49:31 -0800

    open close

--- client/src/components/CheckoutButton/CheckoutButton.js
+++ client/src/components/CheckoutButton/CheckoutButton.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './CheckoutButton.module.scss';
+import { useContext } from 'react';
+import { FirebaseContext } from '../../firebaseContext';
+import { ref, onValue } from 'firebase/database';
+
 const CheckoutButton = ({ onClick }) => {
+  const database = useContext(FirebaseContext);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Reference to the "restaurants" node in the database
+    const restaurantsRef = ref(database, 'restaurants/-Nj2D9YEjyq1iyZM6aSQ');
+
+    // Attach an asynchronous callback to read the data at the reference
+    onValue(restaurantsRef, (snapshot) => {
+      const data = snapshot.val();
+      const openn = data.open
+      setOpen(openn)
+    });
+
+    // Detach the callback when the component unmounts
+    return () => {
+      // Unsubscribe from the database changes
+      // This helps avoid potential memory leaks
+      // when the component is unmounted
+      onValue(restaurantsRef, () => {});
+    };
+  }, [database]); 
+
+  
   return (
-    <button className={classes.checkoutButton} onClick={onClick}>
+    <button className={!open ? classes.disabled: classes.checkoutButton} onClick={onClick} disabled={!open}>
       Checkout
     </button>
   );
